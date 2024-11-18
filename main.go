d91bd41bca2d96182ef842a67f0390c8696d27f7
stefan <stefan.holmberg@systementor.se>
2024-11-18 17:47:14 +0100

    docker build

--- main.go
+++ main.go
@@ -1,3 +1,8 @@
+// 1 . DOCKERFILE
+// 2. Buildspec som bygger DOCKERFILE
+// 2.5           docker login git.systementor.se yacloud yacloud1
+// 3.               PUSH IMAGE -> git.systementor.se
+
 package main
 
 import (
